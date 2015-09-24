dda9f75f2c5043118c68da523e6f7cf0fdd7b978
Francois Hill (home win10) <francoishill11@gmail.com>
2015-09-24 11:10:22 +0200

    Added more validation methods.

--- Implementations/Misc/Validation/DefaultBaseValidationService/service.go
+++ Implementations/Misc/Validation/DefaultBaseValidationService/service.go
@@ -1,7 +1,7 @@
 package DefaultBaseValidationService
 
 import (
-	"github.com/asaskevich/govalidator"
+	gov "github.com/asaskevich/govalidator"
 	"strings"
 
 	. "github.com/francoishill/golang-common-ddd/Interface/Misc/Validation"
@@ -9,9 +9,37 @@ import (
 
 type service struct{}
 
-func (s *service) IsEmail(str string) bool {
-	return govalidator.IsEmail(str) && len(strings.TrimSpace(str)) >= 3
+func (s *service) extendedIsEmail(str string) bool {
+	return gov.IsEmail(str) && len(strings.TrimSpace(str)) >= 3
 }
+
+//Public methods
+func (s *service) InRange(value, left, right float64) bool   { return gov.InRange(value, left, right) }
+func (s *service) IsASCII(str string) bool                   { return gov.IsASCII(str) }
+func (s *service) IsAlpha(str string) bool                   { return gov.IsAlpha(str) }
+func (s *service) IsAlphanumeric(str string) bool            { return gov.IsAlphanumeric(str) }
+func (s *service) IsBase64(str string) bool                  { return gov.IsBase64(str) }
+func (s *service) IsCreditCard(str string) bool              { return gov.IsCreditCard(str) }
+func (s *service) IsEmail(str string) bool                   { return s.extendedIsEmail(str) }
+func (s *service) IsEmptyStringOrWhitespace(str string) bool { return strings.TrimSpace(str) == "" }
+func (s *service) IsFilePath(str string) (bool, int)         { return gov.IsFilePath(str) }
+func (s *service) IsFloat(str string) bool                   { return gov.IsFloat(str) }
+func (s *service) IsHexadecimal(str string) bool             { return gov.IsHexadecimal(str) }
+func (s *service) IsHexcolor(str string) bool                { return gov.IsHexcolor(str) }
+func (s *service) IsIP(str string) bool                      { return gov.IsIP(str) }
+func (s *service) IsIPv4(str string) bool                    { return gov.IsIPv4(str) }
+func (s *service) IsIPv6(str string) bool                    { return gov.IsIPv6(str) }
+func (s *service) IsInt(str string) bool                     { return gov.IsInt(str) }
+func (s *service) IsJSON(str string) bool                    { return gov.IsJSON(str) }
+func (s *service) IsLatitude(str string) bool                { return gov.IsLatitude(str) }
+func (s *service) IsLongitude(str string) bool               { return gov.IsLongitude(str) }
+func (s *service) IsNatural(value float64) bool              { return gov.IsNatural(value) }
+func (s *service) IsNegative(value float64) bool             { return gov.IsNegative(value) }
+func (s *service) IsNonNegative(value float64) bool          { return gov.IsNonNegative(value) }
+func (s *service) IsNonPositive(value float64) bool          { return gov.IsNonPositive(value) }
+func (s *service) IsNumeric(str string) bool                 { return gov.IsNumeric(str) }
+func (s *service) IsPositive(value float64) bool             { return gov.IsPositive(value) }
+func (s *service) IsWhole(value float64) bool                { return gov.IsWhole(value) }
 
 func New() BaseValidationService {
 	return &service{}
--- Interface/Misc/Validation/BaseValidationService.go
+++ Interface/Misc/Validation/BaseValidationService.go
@@ -1,5 +1,30 @@
 package Validation
 
 type BaseValidationService interface {
+	InRange(value, left, right float64) bool
+	IsASCII(str string) bool
+	IsAlpha(str string) bool
+	IsAlphanumeric(str string) bool
+	IsBase64(str string) bool
+	IsCreditCard(str string) bool
 	IsEmail(str string) bool
+	IsEmptyStringOrWhitespace(str string) bool
+	IsFilePath(str string) (bool, int)
+	IsFloat(str string) bool
+	IsHexadecimal(str string) bool
+	IsHexcolor(str string) bool
+	IsIP(str string) bool
+	IsIPv4(str string) bool
+	IsIPv6(str string) bool
+	IsInt(str string) bool
+	IsJSON(str string) bool
+	IsLatitude(str string) bool
+	IsLongitude(str string) bool
+	IsNatural(value float64) bool
+	IsNegative(value float64) bool
+	IsNonNegative(value float64) bool
+	IsNonPositive(value float64) bool
+	IsNumeric(str string) bool
+	IsPositive(value float64) bool
+	IsWhole(value float64) bool
 }
