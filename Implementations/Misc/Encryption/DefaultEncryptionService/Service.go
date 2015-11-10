c89c2cbb2a6ac6aeca031bc7065649652cc2fcc0
Francois Hill (home win10) <francoishill11@gmail.com>
2015-11-10 17:10:08 +0200

    Added couple of `GenerateRandomTokenString...` functions into EncryptionService.

--- Implementations/Misc/Encryption/DefaultEncryptionService/Service.go
+++ Implementations/Misc/Encryption/DefaultEncryptionService/Service.go
@@ -1,6 +1,8 @@
 package DefaultEncryptionService
 
 import (
+	"crypto/rand"
+
 	. "github.com/francoishill/golang-common-ddd/Interface/Misc/Encryption"
 )
 
@@ -12,6 +14,31 @@ func (d *defaultEncryption) PasswordMatchHex(guessedPassword, hexSalt, hexHashed
 
 func (d *defaultEncryption) CreatePassword(raw_pass string) Password {
 	return CreatePassword_DefaultComplexity(raw_pass)
+}
+
+func (d *defaultEncryption) generateRandomString(length int, dictionary string) string {
+	var bytes = make([]byte, length)
+	rand.Read(bytes)
+	for k, v := range bytes {
+		bytes[k] = dictionary[v%byte(len(dictionary))]
+	}
+	return string(bytes)
+}
+
+func (d *defaultEncryption) GenerateRandomTokenString_AlphaNumeric(length int) string {
+	return d.generateRandomString(length, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+}
+
+func (d *defaultEncryption) GenerateRandomTokenString_Alpha(length int) string {
+	return d.generateRandomString(length, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+}
+
+func (d *defaultEncryption) GenerateRandomTokenString_Numeric(length int) string {
+	return d.generateRandomString(length, "0123456789")
+}
+
+func (d *defaultEncryption) GenerateRandomTokenString_SpecifyCharlist(length int, charlist string) string {
+	return d.generateRandomString(length, charlist)
 }
 
 func New() EncryptionService {
--- Interface/Misc/Encryption/EncryptionService.go
+++ Interface/Misc/Encryption/EncryptionService.go
@@ -8,4 +8,9 @@ type Password interface {
 type EncryptionService interface {
 	PasswordMatchHex(guessedPassword, hexSalt, hexHashedPassword string) bool
 	CreatePassword(raw_pass string) Password
+
+	GenerateRandomTokenString_AlphaNumeric(length int) string
+	GenerateRandomTokenString_Alpha(length int) string
+	GenerateRandomTokenString_Numeric(length int) string
+	GenerateRandomTokenString_SpecifyCharlist(length int, charlist string) string
 }
